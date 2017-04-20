0863464646625776455ef0d88fd361d416f69017
vanthe14 <vanthe14@gmail.com>
2017-04-20 14:05:05 +0700

    hello the

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",function(req,res){
-  res.render("home");
+  //res.render("home");
+  res.send("Hello The");
 });
 
 
