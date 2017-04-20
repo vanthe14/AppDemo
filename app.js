996f524993c6b8e503e9a6eb805c9d07bf80422a
vanthe14@gmail.com <levanthe14020506>
2017-04-20 13:24:38 +0700

    dasdas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express    = require("express");
-
+var WebSocketServer = require("ws").Server
 var path = require('path');
 var http = require('http');
 // var fs = require('fs');
@@ -21,7 +21,8 @@ app.get('/page_full_width', pages.page_full_width);
 
 
 // some environment variables
-app.set('port', 'https://taplamweb.herokuapp.com/');
+
+app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -33,5 +34,25 @@ app.get("/",function(req,res){
 
 
 //http.createServer(app).listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
+ // console.log('Express server listening on port ' + app.get('port'));
 //});
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
